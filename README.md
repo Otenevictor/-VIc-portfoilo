c224a04248c26b45aa1d20951dc164eeea80fa4f
Otenevictor <solomonotene36@gmail.com>
2023-01-30 09:57:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-portfoilo
+ about me
---
+++ script.js
@@ -0,0 +1,18 @@
+const date = new Date();
+const theYear = date.getFullYear();
+console.log(theYear)
+const year = document.getElementById("year").innerHTML = theYear
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
