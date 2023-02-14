cc98aa06913c744cec97a76d2fc5f8230102ff79
Otenevictor <solomonotene36@gmail.com>
2023-02-14 03:26:57 +0100

    added animation

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,2],
+    easing: "easeInOutQuad",
+    duration: 1000,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 2000
+  });
